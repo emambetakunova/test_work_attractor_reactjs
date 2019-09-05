c9ac9133b294488e28f939e633f0955a7cb376c8
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-09-06 01:24:55 +0600

    added input for message and state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const shift = 3;
+
+class App extends Component {
+  state = {
+    message: '',
+    result: ''
+  };
+
+  inputChangeHandler = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  };
+
+  cipher = () => {
+    const plainText = this.state.message;
+    let encodedText = "";
+    for(let i = 0; i < plainText.length; i++) {
+      const letter = plainText[i];
+      const index = alphabet.indexOf(letter);
+      if (index === -1) {
+        encodedText += letter;
+      } else {
+        const newIndex = index + shift;
+        let newLetter = alphabet[newIndex];
+        if (newLetter === undefined) {
+          let indexFromStart = newIndex - alphabet.length;
+          newLetter = alphabet[indexFromStart];
+
+        }
+        encodedText += newLetter;
+      }
+    }
+
+    this.setState({result: encodedText})
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p>Enter message:</p>
+        <input
+          required
+          type="text"
+          name="message"
+          placeholder="Enter message"
+          value={this.state.message}
+          onChange={this.inputChangeHandler}
+        />
+        <button onClick={this.cipher}>Encrypt</button>
+        <p>Result: </p>
+        <span className="boxResult">{this.state.result}</span>
+      </div>
+    )
+  }
 }
 
 export default App;
